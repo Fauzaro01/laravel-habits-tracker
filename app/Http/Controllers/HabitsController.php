b32481b9776865dec2aa73f8f3abdd13d07877b1
Fauzaro01 <muhamadfauzan4750@gmail.com>
2024-02-05 12:17:36 +0700

    + Menambahkan Read and Create walaupun belum ada view nya

---
+++ app/Http/Controllers/HabitsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\habits;
+
+class HabitsController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function index() {
+        return view('habit.index');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|max:250',
+            'description' => "max:500"
+        ]);
+
+        habits::insert([
+            'id' => Str::random(12),
+            'name' => $request->name,
+            'decription' => $request->description|null,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect('habits.index')->withSuccess('Berhasil menambahkan Habits baru!');
+    }
+}
